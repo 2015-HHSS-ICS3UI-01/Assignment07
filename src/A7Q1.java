071b07aa6ed926632af35407fdde349b71d63118
richj0985 <richj0985@HRH-IC0027343.wrdsb.ca>
2015-11-26 14:18:36 -0500

    School Changes 2

--- src/A7Q1.java
+++ src/A7Q1.java
@@ -15,25 +15,37 @@ public class A7Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // create a program that will determine the average of a class based on the percentages given
+                
         // create a scanner for input
         Scanner input = new Scanner(System.in);
-
+        
+        System.out.println("Enter the following information to determine the average of a computer science class.");
+        
+        // ask the user how many students are in the class and store this in a variable
+        System.out.print("Enter how many students are in the class: ");
+        int numberOfStudents = input.nextInt();
+        
+        // create an array to store the percentages of the students
         double[] aryMarks;
         
-        aryMarks = new double[10];
+        // the number of variables stored is the number of students in the class
+        aryMarks = new double[numberOfStudents];
         
-        aryMarks[0] = 100.0;
-        aryMarks[1] = 100.0;
-        aryMarks[2] = 100.0;
-        aryMarks[3] = 100.0;
-        aryMarks[4] = 100.0;
-        aryMarks[5] = 100.0;
-        aryMarks[6] = 100.0;
-        aryMarks[7] = 100.0;
-        aryMarks[8] = 100.0;
-        aryMarks[9] = 100.0;
+        // create a variable to store the average
+        double average = 0;
         
-        double average = (aryMarks[0] + aryMarks[1] + aryMarks[2] + aryMarks[3] + aryMarks[4] + aryMarks[5] + aryMarks[6] + aryMarks[7] + aryMarks[8] + aryMarks[9]) / 10;
-        System.out.println(average);
+        // ask the user to enter the percentaages
+        System.out.println("Please enter the marks of the students (1 per line): ");
+        
+        // count the number of variables to store a certain number of marks
+        for(int index = 0; index < numberOfStudents; index = index + 1){
+            aryMarks[index] = input.nextDouble();
+            average = average + aryMarks[index];
+        }
+        
+        average = average / numberOfStudents;
+        
+        System.out.println("The average of the class is " + average + ".");
     }
 }
