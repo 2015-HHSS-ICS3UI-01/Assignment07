840130975a7e34348794d68d61de049785f9fd70
butlj9437 <butlj9437@HRH-IC0027341.wrdsb.ca>
2015-11-30 14:25:06 -0500

    ..

--- src/A7Q2.java
+++ src/A7Q2.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -13,6 +16,24 @@ public class A7Q2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("How many people?");
+        Scanner input = new Scanner(System.in);
+        int ppl = input.nextInt();
+        System.out.println("What are their high?");
+        int[] height = new int[ppl];
+        for(int i = 0; i < ppl ; i++){
+            height[i] = input.nextInt();
+        }
+        int avg = 0;
+        for(int i = 0; i < ppl ; i++){
+            avg = avg + height[i];
+        }
+        avg = avg / ppl;
+        System.out.println("The heights above average are:");
+        for(int i = 0; i < ppl ; i++){
+            if(height[i]>avg){
+                System.out.println(height[i]);
+            }
+        }
     }
 }
