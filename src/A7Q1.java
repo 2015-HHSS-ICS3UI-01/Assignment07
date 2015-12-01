b05122fe9d080d79ea39fb1c32c08b2c24cdd207
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2015-12-01 12:56:01 -0500

    l

---
+++ src/A7Q1.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author sevcm7279
+ */
+public class A7Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //make a scanner
+        Scanner input = new Scanner(System.in);
+
+        //enter the number of people in the class
+        System.out.println("Enter the number of students.");
+        int a = input.nextInt();
+
+        //create an array
+        int[] marks = new int[a];
+
+        //create a for loop to enter all of the grades
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println("Please enter the grade.");
+            marks[i] = input.nextInt();
+
+        }
+        //add up all of the grades
+        int total = 0;
+        for (int i = 0; i < marks.length; i++) {
+            total = total + marks[i];
+        }
+        //calculate the average
+        double average = total / a;
+
+        //display the average
+        System.out.println("The average grade of the class is " + average);
+    }
+}
