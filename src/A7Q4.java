a494c1c73f827f057651e9c9e9b106de39b22b73
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2015-12-04 13:34:58 -0500

    l

--- src/A7Q4.java
+++ src/A7Q4.java
@@ -5,47 +5,46 @@ import java.util.Scanner;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author sevcm7279
  */
 public class A7Q4 {
-    
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         //create a scanner
         Scanner input = new Scanner(System.in);
-        
+
         //create an array
-        int[] marks = new int [4];
-        
+        int[] marks = new int[4];
+
         //create a loop to enter the marks
-        for (int e = 0; e < marks.length; e++){
+        for (int e = 0; e < marks.length; e++) {
             System.out.println("Please enter a mark.");
             marks[e] = input.nextInt();
         }
         //create a loop to check every number in array
-        for (int i = 0; i < marks.length; i++){
-            
-                for (int a = 0; a < marks.length -1; a++){
-            int x = marks[i];     
-            int y = marks[a + 1];
-            
-            if (marks[i] > marks [a + 1]){
-                marks[i] = y;
-                marks[a + 1] = x;
-        
-                    
+        for (int i = 0; i < marks.length - 1; i++) {
+
+            for (int a = i + 1; a < marks.length; a++) {
+                int x = marks[i];
+                int y = marks[a];
+
+                if (marks[i] > marks[a]) {
+                    marks[i] = y;
+                    marks[a] = x;
                 }
             }
-        
-        
-        for (int o = 0; o < marks.length; o++){
+        }
+
+        System.out.println("");
+        for (int o = 0; o < marks.length; o++) {
             System.out.println(marks[o]);
-       
+
+        }
+
     }
-}}}
+}
