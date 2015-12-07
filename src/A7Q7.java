c8d878439d000decc87f9510ecd8a174d209eb0d
butlj9437 <butlj9437@HRH-IC0027341.wrdsb.ca>
2015-12-07 13:20:43 -0500

    ..

---
+++ src/A7Q7.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author butlj9437
+ */
+public class A7Q7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int p = 2;
+        boolean[] prime = new boolean[1000];
+        
+    }
+}
