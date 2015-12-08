177cea8f8e78ed6d56f64d946f2b52519b887e1b
butlj9437 <butlj9437@HRH-IC0027341.wrdsb.ca>
2015-12-08 14:25:13 -0500

    ..

--- src/A7Q7.java
+++ src/A7Q7.java
@@ -15,6 +15,26 @@ public class A7Q7 {
     public static void main(String[] args) {
         int p = 2;
         boolean[] prime = new boolean[1000];
-        
+        for(int i = 0; i <= 1000; i++){
+            prime[i] = true;
+        }
+        prime[0] = false;
+        while(p*p<=1000){
+            for(int i = 2; p*i<=1000; i++){
+                prime[(i*p)-1] = false;
+            }
+            boolean check = true;
+            for(int i = p; check; i++){
+                if(prime[i]){
+                    p = i + 1;
+                    check = false;
+                }
+            }
+        }
+        for(int i = 1; i<=1000; i++){
+            if(prime[i]){
+                System.out.println(i+1);
+            }
+        }
     }
 }
