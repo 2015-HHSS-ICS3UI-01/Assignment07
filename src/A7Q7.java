068359361db61727a3d00e4bf33094f616db81bb
lalim9800 <lalim9800@HRH-IC0027329.wrdsb.ca>
2015-12-15 12:25:50 -0500

    A7 FINAL

--- src/A7Q7.java
+++ src/A7Q7.java
@@ -14,10 +14,10 @@ public class A7Q7 {
      */
     public static void main(String[] args) {
         //set array with 1000 numbers (because 0 is counted as a number)
-         int[] prime = new int[1001];
+         int[] primenumber = new int[1001];
         //loop through 1000 times 
         for (int i = 2; i < 1001; i++) { 
-            prime[i] = i;   //the numbers are enetred into the array
+            primenumber[i] = i;   //the numbers are enetred into the array
         }
 
         //make the integer and set the value to zero
@@ -30,7 +30,7 @@ public class A7Q7 {
                 //if k is less than 1001
                 if (k < 1001) {   
                     //make the value a negative 
-                   prime [k] = k * -1; 
+                   primenumber [k] = k * -1; 
                 }
             }
         }//tell user the prime numbers 
@@ -38,9 +38,9 @@ public class A7Q7 {
         //loop up to 999 
         for (int t = 0; t < 1000; t++) { 
             //if the number is greater than zero (posotive)
-            if (prime[t] > 0) { 
+            if (primenumber[t] > 0) { 
                 //out put all the numbers to the user 
-                System.out.print("("+prime[t]+")" + " "); 
+                System.out.print("("+primenumber[t]+")" + " "); 
             }
         }
     }
