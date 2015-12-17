ddaa6fa44ec6874ae26754adeb79c48bf93453e1
watsk8668 <watsk8668@HRH-IC0027333.wrdsb.ca>
2015-12-17 13:18:32 -0500

    A7Q1,Q2,Q3,Q4,Q5  done

--- src/A7Q4.java
+++ src/A7Q4.java
@@ -35,18 +35,22 @@ public class A7Q4 {
 
         //sort the marks from lowest to highest
         for (int x = 0; x < order.length - 1; x++) {
-            for (int y = 0; y < order.length + 1; y++) {
-                int temp1 = order[x];
-                int temp2 = order[y];
+            for (int y = x + 1; y < order.length; y++) {
+                int temp = order[x];
                 if (order[x] > order[y]) {
-                    order[x] = temp2;
-                    order[y] = temp1;
+                    order[x] = order[y];
+                    order[y] = temp;
                 }
             }
         }
 
         //the marks in order are
-        System.out.println("The marks from lowest to highest are: " + order[0] + order[1] + order[2] + order[3] + order[4] + order[5] + order[6] + order[7] + order[8] + order[9]);
+        System.out.println("The marks from lowest to highest are: ");
+        
+        //print out the values
+        for(int i = 0; i < order.length; i++) {
+            System.out.println(order[i]);
+        }
 
 
     }
--- src/A7Q5.java
+++ src/A7Q5.java
@@ -1,8 +1,10 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author watsk8668
@@ -13,6 +15,55 @@ public class A7Q5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // import scanner
+        Scanner input = new Scanner(System.in);
+
+        //how many people
+        System.out.println("How many numbers?");
+        int numbers = input.nextInt();
+
+        //create array
+        int[] order = new int[numbers];
+
+        System.out.println("Enter " + numbers + " numbers: ");
+
+        //enter numbers
+        for (int i = 0; i < order.length; i++) {
+            order[i] = input.nextInt();
+        }
+
+        //sort the marks from lowest to highest
+        for (int x = 0; x < order.length - 1; x++) {
+            for (int y = x + 1; y < order.length; y++) {
+                int temp = order[x];
+                if (order[x] > order[y]) {
+                    order[x] = order[y];
+                    order[y] = temp;
+                }
+            }
+        }
+
+        //the marks in order are
+        System.out.println("The marks from lowest to highest are: ");
+
+        //print out the values
+        for (int i = 0; i < order.length; i++) {
+            System.out.println(order[i]);
+        }
+
+        //calculate the median
+        int half = numbers / 2;
+        int end = half % 10;
+        double median = order[half];
+        double media = 90;
+        if (end == 2 || end == 4 || end == 6 || end == 8 || end == 0) {
+            //print out the median
+            System.out.println("The median is " + median);
+        } else {
+            System.out.println("The media is " + media);
+        }
+
+
+
     }
 }
