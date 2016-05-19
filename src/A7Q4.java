c22bcb929e1cd2734663767fdfbf25dfe1aa4ead
halll7908 <halll7908@HRH-IC0027342.wrdsb.ca>
2016-05-19 09:11:02 -0400

    Assignment 7 Progress.

--- src/A7Q4.java
+++ src/A7Q4.java
@@ -1,8 +1,10 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author halll7908
@@ -14,5 +16,13 @@ public class A7Q4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+
+        int[] marks = new int[10];
+
+        for (int i = 0; i < marks.length; i++) { // set array's variable values
+            marks[i] = input.nextInt();
+        }
+        
     }
 }
