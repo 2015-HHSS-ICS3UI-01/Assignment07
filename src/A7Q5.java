9283bbbf8d8a2cfd778cb4db397e929b446bb3ec
halll7908 <halll7908@HRH-IC0027342.wrdsb.ca>
2016-05-20 08:52:58 -0400

    Assignment 7 Finished.

--- src/A7Q5.java
+++ src/A7Q5.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -14,5 +17,34 @@ public class A7Q5 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many marks are being entered?");
+        int marksNum = input.nextInt();
+        int marks[] = new int[marksNum];
+
+        System.out.println("Please enter the " + marksNum + " marks on separate lines.");
+        for (int i = 0; i < marks.length; i++) { // set array's variable values
+            marks[i] = input.nextInt();
+        }
+        
+        // code comparing the smallest
+        for (int ii = 0; ii < marks.length; ii++) {
+            for (int i = 0; i < (marksNum - 1); i++) {
+                int nxt = i + 1; // takes next variable
+                int order; // holds variable
+                if (marks[i] > marks[nxt]) { // ascending sort
+                    order = marks[i]; // swap elements
+                    marks[i] = marks[nxt];
+                    marks[nxt] = order;
+                }
+            }
+        }
+        
+        // print out in ascending order
+        System.out.println("Least to Greatest: ");
+        for (int asc = 0; asc < marksNum; asc++) {
+            System.out.println(marks[asc]);
+        }
     }
 }
