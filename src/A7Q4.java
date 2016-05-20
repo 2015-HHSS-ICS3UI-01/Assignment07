fc4494495f3724fa1250cb1b9d8d685418fbc357
halll7908 <halll7908@HRH-IC0027342.wrdsb.ca>
2016-05-20 08:47:39 -0400

    Assignment 7 Progress.

--- src/A7Q4.java
+++ src/A7Q4.java
@@ -20,9 +20,26 @@ public class A7Q4 {
 
         int[] marks = new int[10];
 
+        System.out.println("Please enter 10 marks on separate lines.");
         for (int i = 0; i < marks.length; i++) { // set array's variable values
             marks[i] = input.nextInt();
         }
-        
+        // code comparing the smallest
+        for (int ii = 0; ii < marks.length; ii++) {
+            for (int i = 0; i < 9; i++) {
+                int nxt = i + 1; // takes next variable
+                int order; // holds variable
+                if (marks[i] > marks[nxt]) { // ascending sort
+                    order = marks[i]; // swap elements
+                    marks[i] = marks[nxt];
+                    marks[nxt] = order;
+                }
+            }
+        }
+        // print out in ascending order
+        System.out.println("Least to Greatest: ");
+        for (int asc = 0; asc < 10; asc++) {
+            System.out.println(marks[asc]);
+        }
     }
 }
