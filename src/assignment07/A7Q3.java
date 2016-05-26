6628da9343c344a7e584e9955a8f8c452dab595a
fostp4040 <fostp4040@HRH-IC0027333.wrdsb.ca>
2016-05-26 08:15:45 -0400

    ​Wrote an array to arrange two integers in ascending order

---
+++ src/assignment07/A7Q3.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment07;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author fostp4040
+ */
+public class A7Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter two integers:");
+        int[] A=new int[2];
+        for(int i=0; i<2; i++){
+            A[i]=input.nextInt();
+        }
+        if(A[0]<A[1]){
+            A[0]=A[1];
+            A[1]=A[0];
+        }
+            System.out.print("The integers in ascending order are: "+A[0]+" "+A[1]); 
+        
+    }
+}
